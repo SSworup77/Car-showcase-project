f923f48c7cb02abab625536a85539e1fd23e342f
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-04-08 21:56:42 +0545

    Fixed footer misalignment issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero/>
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home_text-container">
+          <h1 className="text-4xl font-extrabold text-black-100">Car Catalogue</h1>
+          <p>Explore the collection of cars</p>
+        </div>
+        <div className="home__fiters">
+          <SearchBar/>
+          <div className="home__filter-container">
+            <CustomFilter/>
+            <CustomFilter/>
+          </div>
+
+        </div>
+
+      </div>
     </main>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,10 +7,7 @@ const Footer = () => {
     <footer className="flex flex-col text-black-100 mt-5 border-top border-gray-700">
         <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
             <div className="flex flex-col justify-start items-start gap-6">
-                <Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
-                <p className="text-base text-gray-700">
-                    Car Hub 2025  |  All rights reserved &copy;
-                </p>
+                <Image src="/logo.svg" alt="logo" width={145} height={25} className="object-contain" />
             </div>
             <div className="footer__links">
                 {footerLinks.map((link)=>(
