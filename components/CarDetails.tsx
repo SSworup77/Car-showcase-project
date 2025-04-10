84705eed76c3ce05efdd522cfe5c76231280ccba
SSworup77 <98733444+SSworup77@users.noreply.github.com>
2025-04-10 21:39:50 +0545

    Minor bug fixes - dialog-box not opening

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,39 +1,48 @@
-"use client"
-import { CarProps } from '@/types';
-import React from 'react'
-import Image from 'next/image';
-import { Fragment } from 'react';
-import { Dialog,Transition,TransitionChild } from '@headlessui/react';
+"use client";
+import { CarProps } from "@/types";
+import React from "react";
+import Image from "next/image";
+import { Fragment } from "react";
+import { Dialog, Transition, TransitionChild } from "@headlessui/react";
 
 interface CarDetailsProps {
-  isOpen:boolean;
-  closeModel:()=>void;
-  car:CarProps
+	isOpen: boolean;
+	closeModel: () => void;
+	car: CarProps;
 }
-const CarDetails = ({isOpen, closeModel,car}:CarDetailsProps) => {
-  return (
-    <>
-      <Transition
-      appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModel} >
-          <TransitionChild
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'>
-            <div className='fixed inset-0 bg-black bg-opacity-25'></div>
-          </TransitionChild>
-          <div className=''>
+const CarDetails = ({ isOpen, closeModel, car }: CarDetailsProps) => {
+	return (
+		<>
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog as="div" className="relative z-10" onClose={closeModel}>
+					<TransitionChild
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
+					>
+						<div className="fixed inset-0 bg-black bg-opacity-25"></div>
+					</TransitionChild>
+					<div className="flex inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center"></div>
+						<TransitionChild
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 scale-95"
+							enterTo="opacity-100 scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100"
+							leaveTo="opacity-0"
+						>
+						</TransitionChild>
+					</div>
+				</Dialog>
+			</Transition>
+		</>
+	);
+};
 
-          </div>
-        </Dialog>
-
-      </Transition>
-    </>
-  )
-}
-
-export default CarDetails
+export default CarDetails;
